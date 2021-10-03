3a766f4c3af3070c0699b1a2aa5ed207f0369a52
DongHwan <ycdoit@naver.com>
2021-10-03 16:05:39 +0900

    boj8958

--- src/main/java/boj8958.java
+++ src/main/java/boj8958.java
@@ -3,22 +3,11 @@ import java.util.Scanner;
 public class boj8958 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
+        System.out.println(sum(sc.nextInt()));
+    }
 
-        for (int i = 0; i < n; i++) {
-            String line = sc.nextLine();
-            int result = 0;
-            int score = 0;
-            for (String str : line.split("")) {
-                if (str.equals("O")) {
-                    score += 1;
-                } else {
-                    score = 0;
-                }
-                result += score;
-            }
-            System.out.println(result);
-        }
+    static int sum(int n) {
+        if (n == 1) return 1;
+        return n + sum(n - 1);
     }
 }
